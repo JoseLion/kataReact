7976293518888feb55bf5383be69657b654a088c
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-06-09 15:06:38 -0500

    - Final

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -5,6 +5,117 @@ export class Exercise1 extends Component {
     super(props);
     this.state = {};
   }
+
+  createMatrix(size) {
+      let n = 1;
+      let array = [];
+
+      for (let i = 0; i < size; i++) {
+          let row = [];
+
+          for (let j = 0; j < size; j++) {
+              row.push(n);
+              n++;
+          }
+
+          array.push(row);
+      }
+
+      return array;
+  }
+
+  moveRigh(matrix, pos, output) {
+      while (pos.x < matrix[pos.y].length) {
+        if (output[output.length - 1] != matrix[pos.y][pos.x]) {
+            if (output.indexOf(matrix[pos.y][pos.x]) > -1) {
+                break;
+            }
+
+            output.push(matrix[pos.y][pos.x]);
+        }
+
+        pos.x++;
+      }
+
+      pos.x--;
+
+      return { pos, output };
+  }
+
+  moveDown(matrix, pos, output) {
+    while (pos.y < matrix.length) {
+      if (output[output.length - 1] != matrix[pos.y][pos.x]) {
+          if (output.indexOf(matrix[pos.y][pos.x]) > -1) {
+              break;
+          }
+
+          output.push(matrix[pos.y][pos.x]);
+      }
+
+      pos.y++;
+    }
+
+    pos.y--;
+
+    return { pos, output };
+  }
+
+  moveLeft(matrix, pos, output) {
+      while (pos.x >= 0) {
+        if (output[output.length - 1] != matrix[pos.y][pos.x]) {
+            if (output.indexOf(matrix[pos.y][pos.x]) > -1) {
+                break;
+            }
+
+            output.push(matrix[pos.y][pos.x]);
+        }
+
+        pos.x--;
+      }
+
+      pos.x++;
+
+      return { pos, output };
+  }
+
+  moveUp(matrix, pos, output) {
+    while (pos.y >= 0) {
+      if (output[output.length - 1] != matrix[pos.y][pos.x]) {
+          if (output.indexOf(matrix[pos.y][pos.x]) > -1) {
+              break;
+          }
+
+          output.push(matrix[pos.y][pos.x]);
+      }
+
+      pos.y--;
+    }
+
+    pos.y++;
+
+    return { pos, output };
+  }
+
+  getOutput(matrix) {
+    let matrixSize = matrix.length * matrix[0].length;
+    let result = {
+        pos: {x: 0, y: 0},
+        output: []
+    }
+
+    while(result.output.length < matrixSize) {
+        result = this.moveRigh(matrix, result.pos, result.output);
+        if (result.output.length >= matrixSize) break;
+        result = this.moveDown(matrix, result.pos, result.output);
+        if (result.output.length >= matrixSize) break;
+        result = this.moveLeft(matrix, result.pos, result.output);
+        if (result.output.length >= matrixSize) break;
+        result = this.moveUp(matrix, result.pos, result.output);
+        if (result.output.length >= matrixSize) break;
+    }
+
+    return result.output;
+  }
   
   render() {
     return (
--- src/Exercise1/specs/Exercise1.spec.js
+++ src/Exercise1/specs/Exercise1.spec.js
@@ -15,5 +15,91 @@ describe('Exercise1', () => {
     it('should defined state', () => {
       expect(instance.state).toEqual({});
     });
+
+    it('create source array', () => {
+        const array = instance.createMatrix(3);
+
+        expect(array[0]).toEqual([1, 2, 3]);
+        expect(array[1]).toEqual([4, 5, 6]);
+        expect(array[2]).toEqual([7, 8, 9]);
+    });
+
+    it('move right', () => {
+        const matrix = instance.createMatrix(3);
+        const pos = {x: 0, y: 0}
+        const currentOutput = [];
+        const result = instance.moveRigh(matrix, pos, currentOutput);
+
+        expect(result).toEqual({
+            pos: {
+                x: 2,
+                y: 0
+            },
+            output: [1, 2, 3]
+        });
+    });
+
+    it('move down', () => {
+        const matrix = instance.createMatrix(3);
+        const pos = {x: 2, y: 0}
+        const currentOutput = [1, 2, 3];
+        const result = instance.moveDown(matrix, pos, currentOutput);
+
+        expect(result).toEqual({
+            pos: {
+                x: 2,
+                y: 2
+            },
+            output: [1, 2, 3, 6, 9]
+        });
+    });
+
+    it('move left', () => {
+        const matrix = instance.createMatrix(3);
+        const pos = {x: 2, y: 2}
+        const currentOutput = [1, 2, 3, 6, 9];
+        const result = instance.moveLeft(matrix, pos, currentOutput);
+
+        expect(result).toEqual({
+            pos: {
+                x: 0,
+                y: 2
+            },
+            output: [1, 2, 3, 6, 9, 8, 7]
+        });
+    });
+
+    it('move up', () => {
+        const matrix = instance.createMatrix(3);
+        const pos = {x: 0, y: 2}
+        const currentOutput = [1, 2, 3, 6, 9, 8, 7];
+        const result = instance.moveUp(matrix, pos, currentOutput);
+
+        expect(result).toEqual({
+            pos: {
+                x: 0,
+                y: 1
+            },
+            output: [1, 2, 3, 6, 9, 8, 7, 4]
+        });
+    });
+
+    it('check output', () => {
+        let matrix = instance.createMatrix(3);
+        let output = instance.getOutput(matrix);
+        expect(output).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+
+        matrix = instance.createMatrix(4);
+        output = instance.getOutput(matrix);
+        expect(output).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+
+        matrix = [
+            [1, 2, 3, 4, 5, 6],
+            [7, 8 ,9, 10, 11, 12],
+            [13, 14, 15, 16, 17, 18]
+        ];
+        output = instance.getOutput(matrix);
+        expect(output).toEqual([1, 2, 3, 4, 5, 6, 12, 18, 17, 16, 15, 14, 13, 7, 8 ,9, 10, 11]);
+    });
   });
 });
